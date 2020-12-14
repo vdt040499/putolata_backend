fe58ba985261ef94a967e39dcf4ff28fc8e5ebc5
Vo Tan <vdt040499@gmail.com>
2020-12-14 16:45:00 +0700

    Fetch product api and render product list

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,36 +1,80 @@
-const shortId = require('shortid');
-const slugify = require('slugify');
-const Product = require('../models/product.model');
+const shortId = require("shortid");
+const slugify = require("slugify");
+
+const Category = require("../models/category.model");
+const Product = require("../models/product.model");
 
 exports.createProduct = (req, res) => {
-    const { 
-        name, price, description, quantity, category}
-    = req.body;
+  const { name, price, description, quantity, category } = req.body;
 
-    let productPictures = [];
+  let productPictures = [];
 
-    if (req.files.length > 0) {
-        productPictures = req.files.map(file => {
-            return { img: file.filename }
-        });
+  if (req.files.length > 0) {
+    productPictures = req.files.map((file) => {
+      return { img: file.filename };
+    });
+  }
+
+  const product = new Product({
+    name: name,
+    slug: slugify(name),
+    price,
+    quantity,
+    description,
+    productPictures,
+    category,
+    createdBy: req.user._id,
+  });
+
+  product.save((error, product) => {
+    if (error) return res.status(400).json({ error });
+
+    if (product) {
+      res.status(201).json({ product });
     }
-    
-    const product = new Product({
-        name: name,
-        slug: slugify(name),
-        price,
-        quantity,
-        description,
-        productPictures,
-        category,
-        createdBy: req.user._id
-    });
+  });
+};
 
-    product.save((error, product) => {
-        if (error) return res.status(400).json({ error });
+exports.getProductsBySlug = (req, res) => {
+  const { slug } = req.params;
+  Category.findOne({ slug: slug })
+    .select("_id")
+    .exec((error, category) => {
+      if (error) {
+        return res.status(400).json({ error });
+      }
 
-        if (product) {
-            res.status(201).json({ product });
-        }
+      if (category) {
+        Product.find({ category: category._id }).exec((error, products) => {
+          if (error) {
+            return res.status(400).json({ error });
+          }
+
+          if (products.length > 0) {
+            res.status(200).json({
+              products,
+              productsByPrice: {
+                under5k: products.filter((product) => product.price <= 5000000),
+                under10k: products.filter(
+                  (product) =>
+                    product.price > 5000000 && product.price <= 10000000
+                ),
+                under15k: products.filter(
+                  (product) =>
+                    product.price > 10000000 && product.price <= 15000000
+                ),
+                under20k: products.filter(
+                  (product) =>
+                    product.price > 15000000 && product.price <= 20000000
+                ),
+                under30k: products.filter(
+                  (product) =>
+                    product.price > 20000000 && product.price <= 30000000
+                ),
+              },
+            });
+          }
+        });
+      }
     });
-}
+};
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,25 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const multer = require('multer');
-const shortId = require('shortid');
-const path = require('path');
+const multer = require("multer");
+const shortId = require("shortid");
+const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.join(path.dirname(__dirname), 'uploads'))
-    },
-    filename: function (req, file, cb) {
-      cb(null, shortId.generate() + "-" + file.originalname)
-    }
+  destination: function (req, file, cb) {
+    cb(null, path.join(path.dirname(__dirname), "uploads"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, shortId.generate() + "-" + file.originalname);
+  },
 });
 
 const upload = multer({ storage });
 
-const Product = require('../models/product.model');
-const { requireSignin, adminMiddleware } = require('../common-middleware');
-const { createProduct } = require('../controllers/product.controller');
+const Product = require("../models/product.model");
+const { requireSignin, adminMiddleware } = require("../common-middleware");
+const { createProduct, getProductsBySlug } = require("../controllers/product.controller");
 
-router.post('/product/create', requireSignin, adminMiddleware, upload.array('productPicture'), createProduct);
+router.post(
+  "/product/create",
+  requireSignin,
+  adminMiddleware,
+  upload.array("productPicture"),
+  createProduct
+);
+router.get("/products/:slug", getProductsBySlug);
 // router.get('/')
 
 module.exports = router;

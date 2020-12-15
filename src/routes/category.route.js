d05050d4dc8e825fb36b32e9fa5ba384674aa2b3
Vo Tan <vdt040499@gmail.com>
2020-12-15 13:08:53 +0700

    Edit categories API

--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -1,24 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const multer = require('multer');
-const shortId = require('shortid');
-const path = require('path');
+const multer = require("multer");
+const shortId = require("shortid");
+const path = require("path");
 
-const { requireSignin, adminMiddleware } = require('../common-middleware');
-const { addCategory, getCategories } = require('../controllers/category.controller');
+const { requireSignin, adminMiddleware } = require("../common-middleware");
+const {
+  addCategory,
+  getCategories,
+  updateCategories,
+} = require("../controllers/category.controller");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.join(path.dirname(__dirname), 'uploads'))
-    },
-    filename: function (req, file, cb) {
-      cb(null, shortId.generate() + "-" + file.originalname)
-    }
+  destination: function (req, file, cb) {
+    cb(null, path.join(path.dirname(__dirname), "uploads"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, shortId.generate() + "-" + file.originalname);
+  },
 });
 
 const upload = multer({ storage });
 
-router.post('/category/create', requireSignin, adminMiddleware, upload.single('categoryImage'), addCategory);
-router.get('/category/getcategory', getCategories);
+router.post(
+  "/category/create",
+  requireSignin,
+  adminMiddleware,
+  upload.single("categoryImage"),
+  addCategory
+);
+router.get("/category/getcategory", getCategories);
+router.post(
+  "/category/update",
+  upload.array("categoryImage"),
+  updateCategories
+);
 
 module.exports = router;

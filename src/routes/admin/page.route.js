2d6599d32b873724a50d63b2a781187512cdc317
Vo Tan <vdt040499@gmail.com>
2020-12-22 21:14:40 +0700

    Product Page

--- src/routes/admin/page.route.js
+++ src/routes/admin/page.route.js
@@ -2,11 +2,12 @@ const express = require('express');
 const { upload, requireSignin, adminMiddleware } = require('../../common-middleware');
 const router = express.Router();
 
-const { createPage } = require('../../controllers/admin/page.controller');
+const { createPage, getPage } = require('../../controllers/admin/page.controller');
 
 router.post('/page/create', requireSignin, adminMiddleware, upload.fields([
     { name: 'banners' },
     { name: 'products' }
 ]), createPage);
+router.get('/page/:category/:type', getPage);
 
 module.exports = router;

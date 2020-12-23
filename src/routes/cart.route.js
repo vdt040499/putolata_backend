c818272205ed970d29d0a59b1b9a55585a30317d
Vo Tan <vdt040499@gmail.com>
2020-12-23 22:15:04 +0700

    LoggedIn User Cart

--- src/routes/cart.route.js
+++ src/routes/cart.route.js
@@ -1,9 +1,27 @@
-const express = require('express');
+const express = require("express");
+const {
+  addItemToCart,
+  addToCart,
+  getCartItems,
+  removeCartItems,
+} = require("../controllers/cart.controller");
+const { requireSignin, userMiddleware } = require("../common-middleware");
 const router = express.Router();
 
-const { requireSignin } = require('../common-middleware/index');
-const { addItemToCart } = require('../controllers/cart.controller');
-
-router.post('/cart/addToCart', requireSignin, addItemToCart);
+router.post(
+  "/user/cart/addtocart",
+  requireSignin,
+  userMiddleware,
+  addItemToCart
+);
+//router.post('/user/cart/addToCartByLogin', requireSignin, userMiddleware, addToCart);
+router.post("/user/getCartItems", requireSignin, userMiddleware, getCartItems);
+//new update
+router.post(
+  "/user/cart/removeItem",
+  requireSignin,
+  userMiddleware,
+  removeCartItems
+);
 
 module.exports = router;

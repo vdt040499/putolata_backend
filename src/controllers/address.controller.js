6f8361d8668a96afb90d3d570c88f4c6b30c101f
Vo Tan <vdt040499@gmail.com>
2020-12-24 19:57:25 +0700

    Order Summary

--- src/controllers/address.controller.js
+++ src/controllers/address.controller.js
@@ -1,39 +1,40 @@
+const address = require("../models/address.model");
 const UserAddress = require("../models/address.model");
 
 exports.addAddress = (req, res) => {
   //return res.status(200).json({body: req.body})
   const { payload } = req.body;
   if (payload.address) {
-    // if (payload.address._id) {
+    if (payload.address._id) {
       UserAddress.findOneAndUpdate(
-        { user: req.user._id },
+        { user: req.user._id, "address._id": payload.address._id },
         {
-          $push: {
-            "address": payload.address,
+          $set: {
+            "address.$": payload.address,
           },
-        }, { new: true, upsert: true }
+        }
       ).exec((error, address) => {
         if (error) return res.status(400).json({ error });
         if (address) {
           res.status(201).json({ address });
         }
       });
-    // } else {
-    //   UserAddress.findOneAndUpdate(
-    //     { user: req.user._id },
-    //     {
-    //       $push: {
-    //         address: payload.address,
-    //       },
-    //     },
-    //     { new: true, upsert: true }
-    //   ).exec((error, address) => {
-    //     if (error) return res.status(400).json({ error });
-    //     if (address) {
-    //       res.status(201).json({ address });
-    //     }
-    //   });
-    // }
+    } else {
+      UserAddress.findOneAndUpdate(
+        { user: req.user._id },
+        {
+          $push: {
+            address: payload.address,
+          },
+        },
+        { new: true, upsert: true }
+      ).exec((error, address) => {
+        if (error) return res.status(400).json({ error });
+        if (address) {
+          res.status(201).json({ address });
+        }
+      });
+    }
   } else {
     res.status(400).json({ error: "Params address required" });
   }

7c8d975948b1181c189a6bac650600679d90ee27
Vo Tan <vdt040499@gmail.com>
2020-12-25 11:32:27 +0700

    Product Page

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -38,41 +38,46 @@ exports.createProduct = (req, res) => {
 exports.getProductsBySlug = (req, res) => {
   const { slug } = req.params;
   Category.findOne({ slug: slug })
-    .select("_id")
+    .select("_id type")
     .exec((error, category) => {
       if (error) {
         return res.status(400).json({ error });
       }
-
       if (category) {
         Product.find({ category: category._id }).exec((error, products) => {
           if (error) {
             return res.status(400).json({ error });
           }
 
-          if (products.length > 0) {
-            res.status(200).json({
-              products,
-              productsByPrice: {
-                under5k: products.filter((product) => product.price <= 5000000),
-                under10k: products.filter(
-                  (product) =>
-                    product.price > 5000000 && product.price <= 10000000
-                ),
-                under15k: products.filter(
-                  (product) =>
-                    product.price > 10000000 && product.price <= 15000000
-                ),
-                under20k: products.filter(
-                  (product) =>
-                    product.price > 15000000 && product.price <= 20000000
-                ),
-                under30k: products.filter(
-                  (product) =>
-                    product.price > 20000000 && product.price <= 30000000
-                ),
-              },
-            });
+          if (category.type) {
+            if (products.length > 0) {
+              res.status(200).json({
+                products,
+                productsByPrice: {
+                  under5k: products.filter(
+                    (product) => product.price <= 5000000
+                  ),
+                  under10k: products.filter(
+                    (product) =>
+                      product.price > 5000000 && product.price <= 10000000
+                  ),
+                  under15k: products.filter(
+                    (product) =>
+                      product.price > 10000000 && product.price <= 15000000
+                  ),
+                  under20k: products.filter(
+                    (product) =>
+                      product.price > 15000000 && product.price <= 20000000
+                  ),
+                  under30k: products.filter(
+                    (product) =>
+                      product.price > 20000000 && product.price <= 30000000
+                  ),
+                },
+              });
+            }
+          } else {
+            res.status(200).json({ products });
           }
         });
       }
